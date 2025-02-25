dee23766fe926a89658ea92c0a913f047b26f382
PedroColombo2003 <pedrocorrea087@gmail.com>
2025-02-25 20:16:07 -0300

    Add files via upload

---
+++ exercicio1_0.0.1.py
@@ -0,0 +1,10 @@
+"""
+Programa Hello
+Autor: Pedro Colombo
+Data 25/02/2025
+Versão 0.0.1
+"""
+
+frase = "hello, World!"
+
+print("Hello, World!")	
---
+++ exercicio1_0.0.2.py
@@ -0,0 +1,21 @@
+"""
+Programa Hello
+Autor: Pedro Colombo
+Data 25/02/2025
+Versão 0.0.2
+"""
+
+# Alocação de memória
+
+
+# Entrada de dados
+
+nome = input("Qual o seu nome? ")
+
+# Processamento de dados
+
+
+
+#Saida de dados
+
+print(f"Hello, {nome}!")
